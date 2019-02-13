4963a78b0d328accd35fbe6dfc4f376a4a87ae2d
PearsonJ <pearsonjoshua13131992@gmail.com>
2019-02-13 14:00:17 -0800

    basically finished stying for all size devices for landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import NavLinks from './Components/NavLinks/NavLinks'
+import Logo from './Components/Logo/Logo'
 import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
+      <div>
       <NavLinks/>
+      <Logo/>
+      </div>
     );
   }
 }
