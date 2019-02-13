5cd1c5ae47efae277d4f15e147a6cce4c2ae2688
PearsonJ <pearsonjoshua13131992@gmail.com>
2019-02-13 13:31:56 -0800

    changed cursor to spying glass, finised navbar, is pretty reponsive at this point. links all work.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import NavLinks from './Components/NavLinks/NavLinks'
+import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div/>
+      <NavLinks/>
     );
   }
 }
