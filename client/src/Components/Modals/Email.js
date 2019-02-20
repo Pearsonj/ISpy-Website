215352ccd9aabb1abb26d454e7941727ee34352e
PearsonJ <pearsonjoshua13131992@gmail.com>
2019-02-20 10:51:54 -0800

    got emails sending to email I made for company, Need to add do some more styling, but core functionality is working and emails are being sent! yay!

--- client/src/Components/Modals/Email.js
+++ client/src/Components/Modals/Email.js
@@ -1,12 +1,66 @@
 import React, { Component } from 'react'
 import './email.css'
+import {Form, FormGroup, Input, Label, Button} from 'reactstrap'
+// import 'bootstrap/dist/css/bootstrap.css'
+import axios from 'axios'
 
-const EmailModal = props => {
+class EmailModal extends Component {
+      constructor(){
+        super()
+        this.state={
+          name: '',
+          phoneNumber: '',
+          message: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit =this.handleSubmit.bind(this)
+      }
+
+      handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+      }
+
+      async handleSubmit(e) {
+        e.preventDefault()
+
+        const {name, phoneNumber, message} = this.state
+
+        const form = await axios.post('/api/form', {
+          name, 
+          phoneNumber,
+          message
+        })
+
+      }
+      render(){
       return (
-        <div className="email" style={{backgroundColor: '#731313fc', color: 'white', height: '500px', overflowY: 'auto', borderRadius: '10px'}}>
-        <h1>This is the Email modal</h1>
-        </div>
+        <Form className="email" onSubmit={this.handleSubmit} style={{backgroundColor: '#731313fc', color: 'white', height: '500px', overflowY: 'auto', borderRadius: '10px'}}>
+          <FormGroup>
+            <Label for="name">Name:</Label>
+            <Input
+              type="text"
+              name="name"
+              onChange={this.handleChange} />
+          </FormGroup>
+          <FormGroup>
+            <Label for="phoneNumber">Phone Number:</Label>
+            <Input
+              type="text"
+              name="phoneNumber"
+              onChange={this.handleChange} />
+          </FormGroup>
+          <FormGroup>
+            <Label for="message">Message:</Label>
+            <Input
+              type="textarea"
+              name="message"
+              onChange={this.handleChange} />
+          </FormGroup>
+          <Button>Submit</Button>
+        </Form>
       );
+      }
     }
 
 export default EmailModal
--- server.js
+++ server.js
@@ -1,7 +1,55 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser")
+const nodemailer = require('nodemailer')
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
+
+app.post('/api/form', (req, res) => {
+  nodemailer.createTestAccount((err, account) => {
+    const htmlEmail = `
+    <h3>Contact Details</h3>
+      <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Phone Number: ${req.body.phoneNumber}</li>
+      </ul>
+      <h3>Message:</h3>
+      <p>${req.body.message}</p>
+    `
+
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+             user: 'ispyemails@gmail.com',
+             pass: 'Joshua13!'
+         }
+     });
+
+    let mailOptions = {
+      from: 'A new inspection',
+      to: 'ispyemails@gmail.com',
+      replyTo: 'give em a call',
+      subject: 'New Inspection',
+      text: req.body.message,
+      html: htmlEmail
+    }
+
+    transporter.sendMail(mailOptions, (err, info) =>{
+      if(err){
+        return console.log(err)
+      }
+
+      console.log('Message Sent: %s', info.message)
+      console.log('Message URL: %s', nodemailer.getTestMessageUrl)
+    })
+
+  })
+
+})
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
