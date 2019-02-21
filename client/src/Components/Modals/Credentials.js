9153961a7612a146e9680b5b1d5ebad293f5bfdf
PearsonJ <pearsonjoshua13131992@gmail.com>
2019-02-21 11:22:09 -0800

    heroku push

--- client/src/Components/Modals/Credentials.js
+++ client/src/Components/Modals/Credentials.js
@@ -7,9 +7,9 @@ const CredentialModal = props => {
         <h2>Business License:</h2>
         <span>2342355lkjlkjasdf</span>
         <h2>Inspector ID:</h2>
-        <span>123456</span>
+        <span>1851</span>
         <h4>Have Questions You Want Answered now? Give us a call.</h4>
-        <span>xxx-xxx-xxxx</span>
+        <span>(425)-770-9872</span>
         </div>
       );
     }
